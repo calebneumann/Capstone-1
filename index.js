735f8da2f019da382c136e8192c29a94c7b0cde9
Caleb Neumann <143125128+calebneumann@users.noreply.github.com>
2024-03-01 22:34:44 -0600

    Switched to js, got special button working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 import OpenAI from "openai";
 
 const openai = new OpenAI();
@@ -15,36 +16,11 @@ async function chatGPT() {
   text.style.display = "block";
 
 }
+*/
 
+function displayText(){
 
-async function displayText(){
-  var text = "yo yo yo this is some text!!!"
-  //document.getElementById("textField").innerHTML = text;
-  //text.style.display = "block";
-
-  var response = await fetch("https://api.chucknorris.io/jokes/random", {
-      method: "GET" // default, so we can ignore
-  })
-  var exam = await response.json();
-  
-  var poop = JSON.stringify(exam, undefined, 2);
-  var obj = JSON.parse(poop);
-  var values = Object.values(obj);
-
-          
-  var el = document.getElementById('content');
-  var content;
-  var rand = Math.floor(Math.random() * 10);
-
-  if  (rand >= 0 && rand < 9) {
-      document.getElementById("textField").textContent = obj.value;
-  }
-  else if  (rand == 9) {
-      //document.getElementById("textField").innerHTML = '<div>HAHA GET RICKROLLED</div><iframe width="280" height="157" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=SXWbX1Ftj_y2tdpO&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
-      document.getElementById("textField").innerHTML = '<div>HAHA GET RICKROLLED</div><video width="320" height="240" autoplay><source src="images/RICKROLL.mp4" type="video/mp4"></video>'
-
-  }
-
+  document.getElementById("textField").textContent = "HEEEELP IM STUCK IN JAVASCRIPT";
   text.style.display = "block";
 
 }
