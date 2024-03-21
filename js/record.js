6d4e1b3071fc9341ea5bef1441c5314076e31559
Caleb Neumann <143125128+calebneumann@users.noreply.github.com>
2024-03-20 20:38:54 -0500

    voice to text now shows on the screen in chatGPT.html

--- js/record.js
+++ js/record.js
@@ -10,38 +10,35 @@ let recording = false;
 
 const filePath = 'voice-to-text.wav';
 let file = "";
-
-function startRecord(){
-
+let sttResponse = "";
 
 
-const writer = new wav.Writer({
-  channels: 1,
-  sampleRate: 16000,
-  bitDepth: 16,
-});
-const mic = record.record({
-  sampleRate: 16000,
-  channels: 1,
-  bitDepth: 16,
-});
+async function startRecord(){
 
-if(recording == true){
+  const writer = new wav.Writer({
+    channels: 1,
+    sampleRate: 16000,
+    bitDepth: 16,
+  });
+  const mic = record.record({
+    sampleRate: 16000,
+    channels: 1,
+    bitDepth: 16,
+  });
 
-console.log('Recording...');
-mic._stream.pipe(writer).pipe(file); //Use mic._stream.pipe for piping
+  if(recording == true){
+
+  console.log('Recording...');
+  mic._stream.pipe(writer).pipe(file); //Use mic._stream.pipe for piping
+  }
+
+  else{
+    mic._stream.pipe(writer).end(); //Use mic._stream.end() for to end recording
+    console.log('Recording stopped.');
+    file.end();
+    await voiceToText(filePath);
+  }
 }
-
-else{
-  mic._stream.pipe(writer).end(); //Use mic._stream.end() for to end recording
-  console.log('Recording stopped.');
-  file.end();
-  voiceToText(filePath);
-}
-}
-
-
-
 
 //Function to transcribe audio
 
@@ -51,8 +48,8 @@ async function voiceToText(filePath) {
     model: "whisper-1",
     language: "en"
   });
-
-  console.log(transcription.text);
+  sttResponse = transcription.text;
+  //console.log(transcription.text);
   fs.unlinkSync(filePath);
 
 }
@@ -61,25 +58,24 @@ async function voiceToText(filePath) {
 // Initialize keypress
 keypress(process.stdin);
 
-function recordThing(rec){
+async function recordThing(rec){
 recording == rec;
 
 
     if(recording == true){ //stop recording
         recording = false;
-
-        startRecord();
         say.speak("Recording has stopped", 'Samantha', 1.2);
+
+        await startRecord();
     }
     else if (recording == false) { //start's recording
       recording = true;
       file = fs.createWriteStream(filePath, { encoding: 'binary' });
 
-      startRecord();
+      await startRecord();
       say.speak("Recording voice", 'Samantha', 1.2);
     }
-
-process.stdin.resume();
+    return sttResponse;
 }
 //recordThing();
 module.exports = { recordThing };
